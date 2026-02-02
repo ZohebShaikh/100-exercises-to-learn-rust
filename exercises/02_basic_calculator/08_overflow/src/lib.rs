4fa4a634caa48cfe2e7c1971b4fd99ff76460e9d
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2026-02-02 18:07:12 +0000

    Checkpoint progress

--- exercises/02_basic_calculator/08_overflow/src/lib.rs
+++ exercises/02_basic_calculator/08_overflow/src/lib.rs
@@ -5,10 +5,12 @@
 // For reasons that we'll explain later, the customization needs to be done in the `Cargo.toml`
 // at the root of the repository, not in the `Cargo.toml` of the exercise.
 
+use std::{num::Saturating};
+
 pub fn factorial(n: u32) -> u32 {
-    let mut result = 1;
+    let mut result= Saturating(1 as u32);
     for i in 1..=n {
-        result *= i;
+        result = result.saturating_mul(i);
     }
     result
 }
