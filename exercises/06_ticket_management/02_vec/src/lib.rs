9e8b95f98d4a08c172236e12efe16cd2d24a33ae
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2026-03-14 13:05:58 +0000

    6 checkpoint

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,13 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let n = n as usize;
+    let mut mem = vec![0,1];
+    for i in 2..=n{
+        mem.push(mem[i-1]+mem[i-2]);
+    }
+    return mem[n];
+
 }
 
 #[cfg(test)]
